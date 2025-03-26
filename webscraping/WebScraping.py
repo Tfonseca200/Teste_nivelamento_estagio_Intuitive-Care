7af8b0042297f7f216a76d1db5b7731612e3243e
Thiago <thiagofonsecaclaudino@gmail.com>
2025-03-26 01:17:30 -0300

    refactor: ajustando validação de verificação conteudo pdf em pasta

--- webscraping/WebScraping.py
+++ webscraping/WebScraping.py
@@ -34,15 +34,26 @@ def baixarArquivos():
 
 
 def compactarEmZip():
-     
-    with zipfile.ZipFile(ZIP_DESTINO, "w") as zipf:
-        for root, _, files in os.walk(PASTA_DESTINO):
-            for file in files:
-                zipf.write(os.path.join(root, file), file)
 
-    print(f"Arquivo ZIP criado: {ZIP_DESTINO}")
+    itens = os.listdir(PASTA_DESTINO)
+    arquivos = []
+
+    for item in itens:
+        caminhoCompleto = os.path.join(PASTA_DESTINO, item) #Caminho completo
+        if os.path.isfile(caminhoCompleto): #Verifica se é um item
+            arquivos.append(item)
+
+    if not arquivos:
+        print("Nenhum arquivo encontrado para compactar. O ZIP não será criado.")
+    
+    else:
+        with zipfile.ZipFile(ZIP_DESTINO, "w") as zipf:
+            for root, _, files in os.walk(PASTA_DESTINO):
+                for file in files:
+                    zipf.write(os.path.join(root, file), file)
+            print(f"Arquivo ZIP criado: {ZIP_DESTINO}")
 
 
 if __name__ == "__main__":
-    baixarArquivos();
-    compactarEmZip();
+    baixarArquivos()
+    compactarEmZip()
